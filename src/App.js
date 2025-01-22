eef55da4c47ad24430e35889946e9ef259cf93ba
Sai Lavanya Pothuru <lavanyachowdary308@gmail.com>
2025-01-22 22:17:03 +0530

    multiple route handlers and use of next

--- src/App.js
+++ src/App.js
@@ -1,60 +1,18 @@
 const express = require("express")
 const app = express();
 
-//This will only handle GET call to /user
-app.get("/user/:userId/:name/:number", (req,res) => {
-    console.log(req.params)
-    res.send({firstName: "Sai", lastName: "Lavanya"})
- })
+// app.use("/route", [rh, rh2], rh3, rh4, rh5)
 
-app.get("/user", (req, res) => {
-    console.log(req.query)
-    res.send({firstName: "Sai", lastName: "Lavanya"});
-})
- app.get("/abc", (req,res) => {
-    res.send({firstName: "Sai", lastName: "Lavanya"})
- })
-
- app.get("/ab+c", (req,res) => {
-    res.send({firstName: "Sai", lastName: "Lavanya"})
- })
-
- app.get("/ab?c", (req,res) => {
-    res.send({firstName: "Sai", lastName: "Lavanya"})
- })
-
- app.get("/ab*c", (req,res) => {
-    res.send({firstName: "Sai", lastName: "Lavanya"})
- })
-
- app.get(/a/, (req,res) => {
-    res.send({firstName: "Sai", lastName: "Lavanya"})
- })
-
-app.get(/.*fly$/, (req,res) => {
-    res.send({firstName: "Sai", lastName: "Lavanya"})
- })
-
-app.get("/hello", (req, res) => {
-    res.send("Hello from server");
-})
-
-app.get("/hello/2", (req, res) => {
-    res.send("Hello from server 2");
-})
-
-app.post("/user", (req, res) => {
-    res.send("Data succesfully saved to db")
-})
-
-app.delete("/user", (req, res) => {
-    res.send("Data succesfully deleted from db")
-})
-
-// This will match all the HTTP method API calls to /test
-app.use('/test', (req,res) => {
-    res.send('Hello from the server!');
-})
+app.use("/user", [(req,res,next)=> {
+    //request handler
+    console.log("handling the route error!!");
+    // res.send("Route handler 1");
+    next();
+},
+(req,res) => {
+    console.log("handling the route error2!!")
+    res.send("Route handler 2");
+}])
 
 app.listen(3001 , () => {
     console.log(`Server is listening on port 3001..`);
