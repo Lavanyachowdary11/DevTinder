95810ad3a8d99f993bd59bf01d7aea49ebf6596d
Sai Lavanya Pothuru <lavanyachowdary308@gmail.com>
2025-01-22 22:15:11 +0530

    Handling routes and request handlers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 const express = require("express")
 const app = express();
-const port = 3001;
 
 //This will only handle GET call to /user
+app.get("/user/:userId/:name/:number", (req,res) => {
+    console.log(req.params)
+    res.send({firstName: "Sai", lastName: "Lavanya"})
+ })
+
 app.get("/user", (req, res) => {
+    console.log(req.query)
     res.send({firstName: "Sai", lastName: "Lavanya"});
+})
+ app.get("/abc", (req,res) => {
+    res.send({firstName: "Sai", lastName: "Lavanya"})
+ })
+
+ app.get("/ab+c", (req,res) => {
+    res.send({firstName: "Sai", lastName: "Lavanya"})
+ })
+
+ app.get("/ab?c", (req,res) => {
+    res.send({firstName: "Sai", lastName: "Lavanya"})
+ })
+
+ app.get("/ab*c", (req,res) => {
+    res.send({firstName: "Sai", lastName: "Lavanya"})
+ })
+
+ app.get(/a/, (req,res) => {
+    res.send({firstName: "Sai", lastName: "Lavanya"})
+ })
+
+app.get(/.*fly$/, (req,res) => {
+    res.send({firstName: "Sai", lastName: "Lavanya"})
+ })
+
+app.get("/hello", (req, res) => {
+    res.send("Hello from server");
+})
+
+app.get("/hello/2", (req, res) => {
+    res.send("Hello from server 2");
 })
 
 app.post("/user", (req, res) => {
@@ -20,6 +56,6 @@ app.use('/test', (req,res) => {
     res.send('Hello from the server!');
 })
 
-app.listen(port , () => {
-    console.log(`App listening on port ${port}..`);
+app.listen(3001 , () => {
+    console.log(`Server is listening on port 3001..`);
 })
