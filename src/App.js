efcb4be71cdd304cab8f1d39bd190eff8002dc06
Sai Lavanya Pothuru <46368765+Lavanyachowdary11@users.noreply.github.com>
2025-01-30 22:49:36 +0530

    update and delete api

--- src/App.js
+++ src/App.js
@@ -58,6 +58,37 @@ app.get("/feed", async (req,res) => {
     }
 })
 
+app.delete("/user", async(req,res) => {
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId)
+        if(!user){
+            res.status(404).send("user not found!")
+        }else {
+            res.send("User deleted successfully")
+        }
+    }catch (err) {
+        res.status(400).send("Something went wrong!!")
+    }   
+})
+
+app.patch("/user", async(req,res) => {
+  const userId = req.body.userId;
+  const data = req.body
+  try{
+    const user = await User.findByIdAndUpdate({_id: userId}, data, {
+        returnDocument: "before"
+    });
+    if(!user){
+        res.status(404).send("User not found!")
+    }else {
+        res.send("User updated successfully")
+    }
+  }catch(err) {
+    res.status(400).send("Something went wrong!!")
+  }
+})
+
 connectDB().then(() => {
     console.log("Database connection established...")
     app.listen(3001 , () => {
