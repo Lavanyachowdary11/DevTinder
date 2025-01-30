e88a4b392a5133f12ef11adb13e00fa49394eb69
Sai Lavanya Pothuru <46368765+Lavanyachowdary11@users.noreply.github.com>
2025-01-30 21:59:34 +0530

    get Api's

--- src/App.js
+++ src/App.js
@@ -17,6 +17,46 @@ app.post("/signup", async (req,res) => {
 
 })
 
+//find only one user
+app.get("/user", async (req,res) => {
+    const userEmail= req.body.emailId;
+    try {
+        const user = await User.findOne({emailId: userEmail})
+        if(!user) {
+            res.status(404).send("user not found!")
+        }else {
+            res.send(user);
+        }
+    }catch(err) {
+        res.status(400).send("Something went wrong..")
+    }
+})
+
+//find user by email
+app.get("/user", async (req,res) => {
+    const userEmail= req.body.emailId;
+    try {
+        const users = await User.find({emailId: userEmail})
+        if(users.length === 0){
+            res.status(404).send("User not found!!");
+        }else {
+            res.send(users);
+        }
+    }catch(err) {
+        res.status(400).send("Something went wrong..")
+    }
+})
+
+//find all the users
+app.get("/feed", async (req,res) => {
+    try {
+        const users = await User.find({})       
+            res.send(users);
+    }catch(err) {
+        res.status(400).send("Something went wrong..")
+    }
+})
+
 connectDB().then(() => {
     console.log("Database connection established...")
     app.listen(3001 , () => {
