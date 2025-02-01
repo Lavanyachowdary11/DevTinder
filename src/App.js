2fc162653d091fed2c8fe1e5f9a53f6bd6817e04
Sai Lavanya Pothuru <46368765+Lavanyachowdary11@users.noreply.github.com>
2025-02-01 15:24:34 +0530

    Api data sanitization

--- src/App.js
+++ src/App.js
@@ -72,10 +72,20 @@ app.delete("/user", async(req,res) => {
     }   
 })
 
-app.patch("/user", async(req,res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async(req,res) => {
+  const userId = req.params?.userId;
   const data = req.body
+  const allowedUpdates = [
+    "photoUrl","about", "gender", "age", "skills"
+  ]
   try{
+    const isUpdateAllowed = Object.keys(data).every((k) => allowedUpdates.includes(k))
+    if(!isUpdateAllowed) {
+      throw new Error("update not allowed");
+    }
+    if(data?.skills.length > 10){
+        throw new Error("Skills cannot be more than 10");
+    }
     const user = await User.findByIdAndUpdate({_id: userId}, data, {
         returnDocument: "before",
         runValidators: "true"
