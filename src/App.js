9ba0bfb08572364aea2f30224c080a12f436f827
Sai Lavanya Pothuru <46368765+Lavanyachowdary11@users.noreply.github.com>
2025-02-03 22:21:42 +0530

    mongoose schemas for user data to optimize

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ const connectDB = require("./config/database")
 const app = express();
 const User = require("./models/user")
 const {validateSignUpData} = require("./utils/validation")
-const bcrypt = require("bcrypt")
 const validator = require('validator')
 const cookieParser = require("cookie-parser")
-const jwt = require('jsonwebtoken')
 const { userAuth } = require("./middlewares/auth")
 
 app.use(express.json())
@@ -45,12 +43,9 @@ app.post("/login", async (req,res) => {
         if(!user){
             throw new Error("Invalid credentials");
         }
-        const isPasswordValid = bcrypt.compare(password, user.password)
+        const isPasswordValid = user.validatePassword(password)
         if(isPasswordValid) {
-            //create a JWt token
-            const token = await jwt.sign({_id: user._id}, "DEV@TINDER$0706", {expiresIn: "1d"});
-
-            //Add the token to cookie and send the response back to the user
+            const token = await user.getJWT()
             res.cookie("token", token, {
                 expires: new Date(Date.now() + 8 * 3600000),
             });
