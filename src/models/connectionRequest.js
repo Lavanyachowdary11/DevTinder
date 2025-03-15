9a734a6bc5c210a429ae31bcda713af5f41386c2
Sai Lavanya Pothuru <46368765+Lavanyachowdary11@users.noreply.github.com>
2025-03-15 12:25:44 +0530

    Queries & indexes in DB & Handled corner cases

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const connectionRequestSchema = new mongoose.Schema({
+    fromUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    toUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: {
+            values: ["ignore", "interested", "accepeted", "rejected"],
+            message: `{value} is incorrect status type`
+        }
+    }
+}, {
+    timestamps: true,
+});
+
+
+connectionRequestSchema.index({fromUserId : 1, toUserId: 1})
+
+connectionRequestSchema.pre("save", function(next) {
+    const connectionRequest = this;
+    //Check if from user id is same as to user id
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+        throw new Error("Cannot send connection request to yourself");
+    }
+    next();
+});
+
+const ConnectionRequestModel = new mongoose.model(
+    "ConnectionRequest",
+    connectionRequestSchema
+);
+
+module.exports = ConnectionRequestModel;
